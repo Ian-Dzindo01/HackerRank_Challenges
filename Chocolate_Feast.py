c3d6ddb0e39015b1b90dc8b6b04daf6444b9ab2c
Ian Dzindo <ian.dzindo01@gmail.com>
2020-01-18 13:00:02 +0100

    Minor addittions.

--- Chocolate_Feast.py
+++ Chocolate_Feast.py
@@ -1,19 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
 def chocolateFeast(n, c, m):
-    wrappers = n/c
-    num = wrappers
+    no = n/c           #3
+    wrappers = no      #3
     while True:
-        if wrappers/m == 1:
-            return num
+        if wrappers//m < 1:
+            return int(no)
 
-        leftoverwrappers = wrappers%m
-        newchocs = int(wrappers/m)
-        num += newchocs
-        wrappers = newchocs + leftoverwrappers
-        print(wrappers)
+        no += wrappers//m              # 3//2 = 1   no je ovde 4
+        newwrappers = wrappers//m      # newwrappers = 1
+        newwrappers += wrappers%m
+        wrappers = newwrappers        # newwrappers = 2 => wrappers = 2
 
-n = 15
-c = 3
-m = 2
 
-result = chocolateFeast(n, c, m)
-print(result)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        ncm = input().split()
+
+        n = int(ncm[0])
+
+        c = int(ncm[1])
+
+        m = int(ncm[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
