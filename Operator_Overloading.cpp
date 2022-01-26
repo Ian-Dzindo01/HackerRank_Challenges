2372f3a396ee77c91d3a9c881d4a49c8080439f2
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-01-26 12:50:30 +0100

    Minor commitments.

---
+++ Operator_Overloading.cpp
@@ -0,0 +1,55 @@
+//Operator Overloading
+
+#include<iostream>
+
+using namespace std;
+
+class Complex
+{
+public:
+    int a,b;
+    void input(string s)
+    {
+        int v1=0;
+        int i=0;
+        while(s[i]!='+')
+        {
+            v1=v1*10+s[i]-'0';
+            i++;
+        }
+        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+        {
+            i++;
+        }
+        int v2=0;
+        while(i<s.length())
+        {
+            v2=v2*10+s[i]-'0';
+            i++;
+        }
+        a=v1;
+        b=v2;
+    }
+};
+
+Complex operator+(Complex a,Complex b)
+    {
+    Complex temp;
+    temp.a = a.a + b.a;
+    temp.b = a.b + b.b;
+    return temp;
+}
+
+ostream& operator<<(ostream& stream,Complex A)
+    {
+    stream<<A.a<<"+i"<<A.b;
+    return stream;
+    }
+
+//Complex Complex::operator<<(Complex x)
+
+//Overload operators + and << for the class complex
+//+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
+//<< should print a complex number in the format "a+ib"
+
+int main()
