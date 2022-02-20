e1292eec7f35c138c746f596e0b459fe097f4a54
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-02-21 00:02:07 +0100

    Solved a couple more.

---
+++ Circular_Array_Rotation.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import deque
+
+
+def circularArrayRotation(a, k, queries):
+    a = deque(a)
+    for i in range(k):
+        a.rotate(1)
+
+    res = [a[i] for i in queries]
+
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    q = int(first_multiple_input[2])
+
+    a = list(map(int, input().rstrip().split()))
+
+    queries = []
+
+    for _ in range(q):
+        queries_item = int(input().strip())
+        queries.append(queries_item)
+
+    result = circularArrayRotation(a, k, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ Non_Divisible_Subset.py
@@ -0,0 +1,4 @@
+# For any number K, the sum of 2 values (A & B) is evenly divisible by K if the sum of the remainders of A/K + B/K is K. Special case when a and b are divisible,
+# giving a sum of 0. For any such remainder, there is 1 and only 1 other remainder value which will make a sum divisible by K.
+
+# Maybe create a list of the pairs to sum in order to get k.
