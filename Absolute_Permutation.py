4dc86bb4dbb7280f4675c8396eb0efc912d85da4
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-02-22 11:22:05 +0100

    Minor progress

--- Absolute_Permutation.py
+++ Absolute_Permutation.py
@@ -1,11 +1,25 @@
-n = 2
-k = 1
+n = 10
+k = 3
 
 l = list(range(n+1))[1:]
-print(l)
-res = []
-for i in l:
-    for j in l:
-        if abs(l[i-1]-j):
-            res.append(j)
-print(res)
+
+def absolutePermutation(n,k):
+    l = list(range(n+1))[1:]
+    res = []
+    for i in l:
+        for j in l:
+            print(l[i-1],j)
+            if abs(j-l[i-1]) == k:
+                res.append(j)
+
+    print(l)
+    print(res)
+    # if len(res) != len(l):
+    #     return [-1]
+
+    return res
+
+
+r = absolutePermutation(n,k)
+# print(l)
+# print(r)
