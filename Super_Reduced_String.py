69a090f4ac9ed8ddd868db026a04972bb6f2c4e7
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-03-02 14:49:40 +0100

    First attempts

---
+++ Super_Reduced_String.py
@@ -0,0 +1,6 @@
+# maybe iterate through the array and delete every occurence of adjacent letters
+# end when string is empty or the program does not find any matches
+# you will probably have to use a while loop
+
+s = 'aaabccddd'
+
