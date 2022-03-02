17a738a64687e253a3ca63e5999eb5b7738e7afb
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-03-02 15:05:22 +0100

    Might work

--- Super_Reduced_String.py
+++ Super_Reduced_String.py
@@ -3,4 +3,19 @@
 # you will probably have to use a while loop
 
 s = 'aaabccddd'
+s = list(s)
 
+while(True):
+    found = False
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            s.pop(i+1)
+            s.pop(i)
+            print(s)
+            found = True
+            break
+
+    if s == '' or found == False:
+        break
+
+print(s)
