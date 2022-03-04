65e9dfecd3539520027663cefc5496eda954a69e
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-03-04 12:37:13 +0100

    Solved Grid Search

--- The_Grid_Search.py
+++ The_Grid_Search.py
@@ -1,32 +1,62 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
 # G - the grid to search
 # P - the pattern to search for
-# loop over each element and see if there is a match
-# if yes continue len(P) times to iterate through the lower rows and see if there is a match with the same indices
-
-G = ['34889246430321978567', '58957542800420926643', '35502505614464308821', '14858224623252492823', '72509980920257761017', '22842014894387119401', '01112950562348692493', '16417403478999610594', '79426411112116726706', '65175742483779283052', '89078730337964397201', '13765228547239925167', '26113704444636815161', '25993216162800952044', '88796416233981756034', '14416627212117283516', '15248825304941012863', '88460496662793369385', '59727291023618867708', '19755940017808628326']
-
-P = ['1641', '7942', '6517', '8907', '1376', '2691', '2599']
-
-n = len(P[0])
-
 def gridSearch(G, P):
-    for i in range(len(G)):
-        for j in range(i):
-            cond = True
-            if G[i][j:j+n] == P[0]:
-                # print('Yes')
-                # print(j, j+n)
-                # print(i)
+    def check(x,y):
+        for i in range(r):
+            if P[i] != G[x+i][y:y+c]:
+                return False
 
-                for a in range(len(P)):
-                    if G[i+a][j:j+n] != P[a]:
-                        cond = False
-                        break
+        return True
 
-                if cond == True:
+
+    for i in range(R):
+        for j in range(C):
+            if G[i][j] == P[0][0]:
+                if check(i,j):
                     return 'YES'
 
     return 'NO'
 
-res = gridSearch(G, P)
-print(res)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        R = int(first_multiple_input[0])
+
+        C = int(first_multiple_input[1])
+
+        G = []
+
+        for _ in range(R):
+            G_item = input()
+            G.append(G_item)
+
+        second_multiple_input = input().rstrip().split()
+
+        r = int(second_multiple_input[0])
+
+        c = int(second_multiple_input[1])
+
+        P = []
+
+        for _ in range(r):
+            P_item = input()
+            P.append(P_item)
+
+        result = gridSearch(G, P)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
