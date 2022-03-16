5cac2a00ab86d37020d77f57ba4585d9b37acf07
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-03-16 20:04:16 +0100

    20th testcase still failing. Fixing tomorrow

--- Python/Separate_the_Numbers.py
+++ Python/Separate_the_Numbers.py
@@ -2,26 +2,28 @@
 # Because the only numbers that will give 1 as a difference are the ones that are 1 bigger than your number
 # Start with the first number, than the first 2, than the first 3 and keep on going like that.
 
-s = '99100'
-n = len(s)
-
 # adjust it to form the same number of integers as the length in the string, not more than that
 # keep on adding numbers until the lengths are the same, dont use the length of the s string because that is wrong
 
 # adjust the return statement as well
+
+s = '99100'
+n = len(s)
+
 def separateNumbers(s):
-    for i in range(1, n):
+    for i in range(1, n//2+1):
         temp = int(s[:i])
 
         t = list(range(temp, temp + n))
         t = ''.join(str(e) for e in t)
-        print(t)
-        print('YES' + ' ' + t[0] if t[:len(s)] == s else 'NO')
-        return 0
+
+        if t[:len(s)] == s:
+            print('YES' + ' ' + str(temp))
+            return 0
+
+
+    print('NO')
+    return 0
+
 
 separateNumbers(s)
-
-
-
-
-
