b3045d880580c13d5c89dc0df0cf5f4c3a42d96c
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-03-19 19:33:30 +0100

    Attempts made

--- Python/Insertion_Sort_Advanced_Analysis.py
+++ Python/Insertion_Sort_Advanced_Analysis.py
@@ -10,20 +10,6 @@ import random
 import re
 import sys
 
-def insertionSort(arr):
-    steps = 0
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i-1
-
-        while j >=0 and key < arr[j] :
-                arr[j+1] = arr[j]
-                j -= 1
-                steps += 1
-
-        arr[j+1] = key
-
-    return steps
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
