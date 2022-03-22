2bc850d076f443c392d8b78620e883396bc71b30
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-03-22 20:36:45 +0100

    Finished Luck Balance

--- Python/Luck_Balance.py
+++ Python/Luck_Balance.py
@@ -1,10 +1,42 @@
-# you got the logic a bit wrong.Rethink
+#!/bin/python3
 
-a = [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]]
-k = 3
-res = 0
+import math
+import os
+import random
+import re
+import sys
 
-res += sum([i[0] for i in a if i[1] == 0])
-res += sum(sorted([i[0] for i in a if i[1] == 1])[:k])
+# 2D array denoting if the amount of luck that can be lost in the contest and whether the contest is important
 
-print(res)
+def luckBalance(k, a):
+    res = 0
+
+    res += sum([i[0] for i in a if i[1] == 0])
+
+    l2 = sorted([i[0] for i in a if i[1] == 1], reverse=True)
+
+    res +=  sum(l2[:k])
+
+    res -= sum(l2[k:])
+
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    contests = []
+
+    for _ in range(n):
+        contests.append(list(map(int, input().rstrip().split())))
+
+    result = luckBalance(k, contests)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
