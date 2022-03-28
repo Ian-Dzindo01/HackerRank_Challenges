58255a1332b3d5d5cf61166a4d8833c9cd430c9f
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-03-28 16:54:51 +0200

    Finished Flipping Bits

--- Python/Flipping_Bits.py
+++ Python/Flipping_Bits.py
@@ -1,5 +1,29 @@
-s = '{:032b}'.format(2147483647)
-print(s)
-print(int(s, 2))
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
 
+def flippingBits(s):
+    s = '{:032b}'.format(s)
+    s = s.replace('1', 't')
+    s = s.replace('0', '1')
+    s = s.replace('t', '0')
+    s = int(s, 2)
+    return s
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
