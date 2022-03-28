83ad4951a80d4de3dabc5d3824ad69c590b2a1cb
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-03-28 17:02:31 +0200

    Added better alternative solution to flipping bits

--- Python/Flipping_Bits.py
+++ Python/Flipping_Bits.py
@@ -1,29 +1,37 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
+# import math
+# import os
+# import random
+# import re
+# import sys
+
+# def flippingBits(s):
+#     s = '{:032b}'.format(s)
+#     s = s.replace('1', 't')
+#     s = s.replace('0', '1')
+#     s = s.replace('t', '0')
+#     s = int(s, 2)
+#     return s
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+#     q = int(input().strip())
+
+#     for q_itr in range(q):
+#         n = int(input().strip())
+
+#         result = flippingBits(n)
+
+#         fptr.write(str(result) + '\n')
+
+#     fptr.close()
+
+
+# Alternative better solution:
+s = '{:032b}'.format(s)
 
 def flippingBits(s):
-    s = '{:032b}'.format(s)
-    s = s.replace('1', 't')
-    s = s.replace('0', '1')
-    s = s.replace('t', '0')
-    s = int(s, 2)
-    return s
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    q = int(input().strip())
-
-    for q_itr in range(q):
-        n = int(input().strip())
-
-        result = flippingBits(n)
-
-        fptr.write(str(result) + '\n')
-
-    fptr.close()
+    maxnum = 4294967295
+    return s ^ maxnum
