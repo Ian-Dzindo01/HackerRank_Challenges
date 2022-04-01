c42961d861c1c3ce2b448196068fc44cfaf37148
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-01 15:46:03 +0200

    Larrys Array Numpy Solution

---
+++ Python/Larry's_Array_Solution_1.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+# Rotations of triples by going backwards each iteration.
+# this one is difficult
+# Maybe traverse the array and see if triplets are descending, if not try to rearrange them using the provided procedure.
+# Implement a special function for the procedure.
+
+
+# Cannot use numpy in the submissions on HackerRank, will upload this to GitHub, but also search for another solution.
+
+A = [1, 6, 5, 2, 3, 4]
+
+def larrysArray(A):
+    while(True):
+        for i in range(len(A)-2):
+            if sorted(A[i:i+3]) != A[i:i+3]:
+                temp = check(A[i:i+3])
+                if temp == 0:
+                    return "NO"
+                else:
+                    A[i:i+3] = temp
+
+            if A == sorted(A):
+                return "YES"
+
+def check(l):
+    if list(np.roll(l, -1)) == sorted(l):
+        return list(np.roll(l, -1))
+    elif list(np.roll(l, -2)) == sorted(l):
+        return list(np.roll(l, -2))
+    elif list(np.roll(l, -3)) == sorted(l):
+        return list(np.roll(l, -3))
+    else:
+        return 0
+
+r = larrysArray(A)
+print(r)
