50d7c728cc24e4a3ee0bcc2ef3585189f2cd9f00
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-01 14:51:09 +0200

    Came up with idea for Larrys Array

---
+++ Python/Larry's_Array.py
@@ -0,0 +1,10 @@
+# Rotations of triples by going backwards each iteration.
+# this one is difficult
+# Maybe traverse the array and see if triplets are descending, if not try to rearrange them using the provided procedure.
+# Implement a special function for the procedure.
+
+A = [1, 6, 5, 2, 3, 4]
+
+def check(s):
+
+
