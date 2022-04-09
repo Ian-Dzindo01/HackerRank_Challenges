d85529353a4d135e7db6f7d89fd0cbfd844fb080
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-09 20:55:27 +0200

    Minor progress

---
+++ Python/Strange_Counter.py
@@ -0,0 +1,42 @@
+# 1  3
+# 2  2
+# 3  1
+
+# 4  6
+# 5  5
+# 6  4
+# 7  3
+# 8  2
+# 9  1
+
+# 10 12
+# 11 11
+# 12 10
+# 13 9
+# 14 8
+# 15 7
+# 16 6
+# 17 5
+# 18 4
+# 19 3
+# 20 2
+# 21 1
+
+# 22 24
+# 23 23
+# 24 22
+
+# 46 48
+# 47 47
+
+# probably going to have a time limitation
+# find and display the counter value at time t
+# find a way to not have to generate the whole list each time
+# maybe identify in which section the number is and work from there
+
+c = 3
+i = 0
+while(True):
+
+
+
